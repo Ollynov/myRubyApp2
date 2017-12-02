a735d5fa0c76bdd6f0c75a5954940ca87c868a8d
Aljosha Novakovic <ollynov@gmail.com>
2017-12-02 21:08:08 +0000

    added a new gift onto my new branch

--- hello.rb
+++ hello.rb
@@ -1,12 +1,13 @@
 gift = [
-          "half of a leftover burrito",
+          "half of a leftover burrito", 
           "a day-old cup of coffee",
           "a tesla",
           "a life-size quadrocopter",
           "a slice of stale wedding cake",
           "a medium-sized island off the coast of Sumatra",
           "a visual prosthesis",
-         ]
+          "my newest gift to the list"
+        ]
 
          
 puts "All I got for valentine's day was #{gift.sample(3)}."
